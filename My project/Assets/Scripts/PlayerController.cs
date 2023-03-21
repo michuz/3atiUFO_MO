9da093ab94e17a93d46ab0c780c3d72837acc7ea
michuz <michuuuzzz1@gmail.com>
2023-03-21 09:40:12 +0100

    Destroying pickups on collision

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -6,16 +6,27 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb2d;
     public float speed;
+    private int licznik;
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    private void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         rb2d.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("PickUp"))
+        {
+            Destroy(collision.gameObject);
+            licznik++;
+            Debug.Log(licznik);
+        }
     }
 }
