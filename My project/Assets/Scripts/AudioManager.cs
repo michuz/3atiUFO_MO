28521961d4bedcc557b19403b9f1977cfced35e9
michuz <michuuuzzz1@gmail.com>
2023-04-04 12:21:28 +0400

    Added sounds, music to assets, AudioManager object, Sound and AudioManager scripts

---
+++ My project/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] music;
+    public Sound[] ufo_move;
+    public Sound[] coin_pickup;
+    public Sound[] wall_hit;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
